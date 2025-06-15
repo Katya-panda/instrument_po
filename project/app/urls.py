31745d4a8cb69585ecb44a6c35d0cbd7031345d7
Katya Polyak <katya_7002@mail.ru>
2025-06-15 13:18:59 +0300

    Лабораторная работа 15

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('author/', views.author, name='author'),
+    path('spec/', views.spec_list, name='spec_list'),
+    path('spec/<int:id>/', views.spec_detail, name='spec_detail'),
+    path('404/', views.error_404, name='error_404'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+import os
 
 # главная страница
 def home(request):
@@ -11,3 +13,43 @@ def about(request):
 # об авторе
 def author(request):
     return render(request, 'app/author.html')
+
+# загрузка квалификаций
+def load_specs():
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'dump.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+# список квалификаций
+def spec_list(request):
+    data = load_specs()
+    specs = []
+    for item in data:
+        if item['model'] == 'data.skill':
+            spec_data = {
+                'id': item['pk'],
+                'name': item['fields']['title'],
+                'code': item['fields']['code'],
+                'desc': item['fields']['desc'] if item['fields']['desc'] else 'Нет описания'
+            }
+            specs.append(spec_data)
+    return render(request, 'app/list.html', {'specs': specs})
+
+# квалификация (подробнее)
+def spec_detail(request, id):
+    data = load_specs()
+    for item in data:
+        if item['model'] == 'data.skill' and item['pk'] == id:
+            spec = {
+                'id': item['pk'],
+                'name': item['fields']['title'],
+                'code': item['fields']['code'],
+                'desc': item['fields']['desc'] if item['fields']['desc'] else 'Нет описания',
+                'all_data': item['fields'] 
+            }
+            return render(request, 'app/detail.html', {'spec': spec})
+    return render(request, 'app/404.html')
+
+# ошибка
+def error_404(request):
+    return render(request, 'app/404.html')
